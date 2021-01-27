8db78197051231ee9f7ae9ddee832695061f1da7
mritd <mritd@linux.com>
2021-01-27 11:43:21 +0800

    feat(conf): update etcd config
    update etcd config
    
    Signed-off-by: mritd <mritd@linux.com>

--- pack/conf/etcd/create.sh
+++ pack/conf/etcd/create.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cfssl gencert --initca=true etcd-root-ca-csr.json | cfssljson --bare etcd-root-ca
-cfssl gencert --ca etcd-root-ca.pem --ca-key etcd-root-ca-key.pem --config etcd-gencert.json etcd-csr.json | cfssljson --bare etcd
+cfssl gencert --initca=true etcd-ca-csr.json | cfssljson --bare etcd-ca
+cfssl gencert --ca etcd-ca.pem --ca-key etcd-ca-key.pem --config etcd-gencert.json etcd-csr.json | cfssljson --bare etcd
